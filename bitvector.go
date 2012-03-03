4b5d326fa7e3c0499ef111a065826ebcbc9a025d
Reiner Pope <reiner.pope@gmail.com>
2012-03-04 08:59:42 +1100

    Remove length checks

--- bitvector.go
+++ bitvector.go
@@ -41,7 +41,7 @@ func size(length int) int {
 }
 
 func (b Bitvector) checkLength(i int) {
-	if i < 0 || i >= b.length {
+/*	if i < 0 || i >= b.length {
 		panic("bitvector index out of range")
-	}
+	}*/
 }
